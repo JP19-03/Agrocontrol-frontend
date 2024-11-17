1692f8a361ef140c19e59f48a5fca0bfb0bc0ec8
JP19-03 <u202014511@upc.edu.pe>
2024-11-17 12:07:21 -0500

    refactor: changes in sidenav to show link depending of user role.
    Co-Authored-By: Alejandro Espino Flores <93212753+CafecitoSKR@users.noreply.github.com>
    Co-Authored-By: Gonzalo Quedena <90155454+GonzaQuedenaUPC@users.noreply.github.com>
    Co-Authored-By: HOLGUINUPC <u202220250@upc.edu.pe>
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>

--- src/app/public/components/sidenav-agricultural-producer/sidenav-agricultural-producer.component.ts
+++ src/app/public/components/sidenav-agricultural-producer/sidenav-agricultural-producer.component.ts
@@ -1,16 +1,19 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {MatIconModule} from "@angular/material/icon";
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../../../profile-management/services/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-sidenav-agricultural-producer',
   standalone: true,
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, NgIf],
   templateUrl: './sidenav-agricultural-producer.component.html',
   styleUrl: './sidenav-agricultural-producer.component.css'
 })
 export class SidenavAgriculturalProducerComponent {
+   @Input() role: string = 'producer';
+
   private router = inject(Router);
   private authService: AuthService = inject(AuthService);
   logOut(): void {
