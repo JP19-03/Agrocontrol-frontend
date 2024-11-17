77986646a9ad72818a077c8c16b8baa89f91d362
JP19-03 <u202014511@upc.edu.pe>
2024-11-17 00:38:27 -0500

    feat(finances): add finance model.
    Co-Authored-By: Alejandro Espino Flores <93212753+CafecitoSKR@users.noreply.github.com>
    Co-Authored-By: Gonzalo Quedena <90155454+GonzaQuedenaUPC@users.noreply.github.com>
    Co-Authored-By: HOLGUINUPC <u202220250@upc.edu.pe>
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>

---
+++ src/app/finances/models/finance.entity.spec.ts
@@ -0,0 +1,7 @@
+import { FinanceEntity } from './finance.entity';
+
+describe('FinanceEntity', () => {
+  it('should create an instance', () => {
+    expect(new FinanceEntity()).toBeTruthy();
+  });
+});
