af864c10283a7242df9c012ff76e4fff5b8f67b5
JP19-03 <u202014511@upc.edu.pe>
2024-11-18 14:04:56 -0500

    feat: add message dialog component.

---
+++ src/app/public/components/message-dialog/message-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import {MatDialogActions, MatDialogContent, MatDialogRef} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-message-dialog',
+  standalone: true,
+  imports: [MatIconModule, NgIf, MatButtonModule, MatDialogActions, MatDialogContent],
+  templateUrl: './message-dialog.component.html',
+  styleUrls: ['./message-dialog.component.css']
+})
+export class MessageDialogComponent {
+  @Input() title: string = '';
+  @Input() message: string = '';
+  @Input() type: 'success' | 'error' = 'success';
+
+  // Injects the MatDialogRef into the component.
+  constructor(private dialogRef: MatDialogRef<MessageDialogComponent>) {}
+
+  // Closes the dialog.
+  closeDialog() {
+    this.dialogRef.close();
+  }
+}
